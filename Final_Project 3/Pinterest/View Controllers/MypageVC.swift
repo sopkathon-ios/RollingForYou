664ad3ba9514d38dd1bf250acd4fe369337852c3
sohyeon <sohyun1018@gmail.com>
2019-05-06 05:37:37 +0900

    mypageVC 바꿈

--- Final_Project 3/Pinterest/View Controllers/MypageVC.swift
+++ Final_Project 3/Pinterest/View Controllers/MypageVC.swift
@@ -10,23 +10,46 @@ import UIKit
 
 class MypageVC: UIViewController {
     
-    var categories = ["Action", "Drama", "Science Fiction", "Kids", "Horror"]
+    var categories = ["2019년 4월 - 5월", "2019년 2월 - 3월", "2019년 1월", "2018년 12월", "2018년 10월 - 11월"]
 
     
-    @IBOutlet var imgBtn: UIButton!
+  @IBOutlet var imgBtn: UIButton! {
+    didSet {
+      imgBtn.layer.cornerRadius = 0.5 * imgBtn.bounds.size.height
+      imgBtn.clipsToBounds = true
+    }
+  }
     @IBOutlet var nameLabel: UILabel!
+    
     @IBOutlet var myMemoriesLabel: UILabel!
     @IBOutlet var ourMemoriesLabel: UILabel!
-    @IBOutlet var myMemoriesBtn: UIButton!
-    @IBOutlet var ourMemoriesBtn: UIButton!
+    @IBOutlet var myMemoriesBtn: UIButton! {
+        didSet {
+            myMemoriesBtn.layer.cornerRadius = 0.2 * myMemoriesBtn.bounds.size.height
+            myMemoriesBtn.clipsToBounds = true
+        }
+    }
+    @IBOutlet var ourMemoriesBtn: UIButton! {
+        didSet {
+            ourMemoriesBtn.layer.cornerRadius = 0.2 * ourMemoriesBtn.bounds.size.height
+            ourMemoriesBtn.layer.borderWidth = 1
+          ourMemoriesBtn.layer.borderColor = UIColor.init(red: 110/255, green: 101/255, blue: 93/255, alpha: 1).cgColor
+            ourMemoriesBtn.clipsToBounds = true
+        }
+    }
     
     @IBOutlet var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-
+        self.navigationController?.isNavigationBarHidden = true
+      self.navigationController?.title = "내 페이퍼"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.navigationController?.isNavigationBarHidden = true
     }
     
     @IBAction func clickImgBtn(_ sender: Any) {
