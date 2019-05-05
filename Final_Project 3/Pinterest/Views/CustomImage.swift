a2f16db1cbef6bfd751601b8fe8ada08aa22bef9
JaeeunAhn <jaeeunno1@naver.com>
2019-05-05 23:54:22 +0900

    add view controller

---
+++ Final_Project 3/Pinterest/Views/CustomImage.swift
@@ -0,0 +1,26 @@
+//
+//  CustomImage.swift
+//  Pinterest
+//
+//  Created by 안재은 on 05/05/2019.
+//  Copyright © 2019 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class CustomImage: UIImageView {
+  required public init?(coder aDecoder: NSCoder) {
+    
+    super.init(coder: aDecoder)
+    
+    layer.cornerRadius = frame.height/2
+    /*
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
+  }
+}
