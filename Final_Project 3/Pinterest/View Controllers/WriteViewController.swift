4d55fcb2fe7bd39ca4f86aab11a943f86585d43d
iamcho2 <thatgrass@naver.com>
2019-05-06 04:33:34 +0900

    add round corner

--- Final_Project 3/Pinterest/View Controllers/WriteViewController.swift
+++ Final_Project 3/Pinterest/View Controllers/WriteViewController.swift
@@ -23,29 +23,32 @@ class WriteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        comment1.layer.cornerRadius = 21;
+        comment1.layer.cornerRadius = 24;
         comment1.layer.borderWidth = 0.5;
         comment1.layer.borderColor = UIColor(red:206/255, green:206/255, blue:206/255, alpha: 1).cgColor;
         
-        comment2.layer.cornerRadius = 21;
+        comment2.layer.cornerRadius = 24;
         comment2.layer.borderWidth = 0.5;
         comment2.layer.borderColor = UIColor(red:206/255, green:206/255, blue:206/255, alpha: 1).cgColor;
         
-    writingComment.layer.cornerRadius = 21;
+    writingComment.layer.cornerRadius = 24;
     writingComment.layer.borderWidth = 0.5;
     writingComment.layer.borderColor = UIColor(red:206/255, green:206/255, blue:206/255, alpha: 1).cgColor;
         
         
         profile1.layer.cornerRadius = 18;
         profile1.layer.borderWidth = 0.5;
+        profile1.clipsToBounds = true
         profile1.layer.borderColor = UIColor(red:206/255, green:206/255, blue:206/255, alpha: 1).cgColor;
         
         profile2.layer.cornerRadius = 18;
         profile2.layer.borderWidth = 0.5;
+        profile2.clipsToBounds = true
         profile2.layer.borderColor = UIColor(red:206/255, green:206/255, blue:206/255, alpha: 1).cgColor;
         
         profile3.layer.cornerRadius = 18;
         profile3.layer.borderWidth = 0.5;
+        profile3.clipsToBounds = true
         profile3.layer.borderColor = UIColor(red:206/255, green:206/255, blue:206/255, alpha: 1).cgColor;
 
 
